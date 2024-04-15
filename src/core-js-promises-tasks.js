fb3665732a31bc5ee606ad6f7e59404366aa82b6
Igor Checheurov <chei@yandex.ru>
2024-04-15 21:53:36 +0300

    feat: add fifth solution

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -89,8 +89,8 @@ function getFirstResolvedPromiseResult(promises) {
  * [promise3, promise6, promise2] => Promise rejected with 2
  * [promise3, promise4, promise6] => Promise rejected with 6
  */
-function getFirstPromiseResult(/* promises */) {
-  throw new Error('Not implemented');
+function getFirstPromiseResult(promises) {
+  return Promise.race(promises).then((result) => result);
 }
 
 /**
@@ -104,8 +104,13 @@ function getFirstPromiseResult(/* promises */) {
  * [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)] => Promise fulfilled with [1, 2, 3]
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)] => Promise rejected with 2
  */
-function getAllOrNothing(/* promises */) {
-  throw new Error('Not implemented');
+function getAllOrNothing(promises) {
+  return Promise.all(promises).then((results) => {
+    return new Promise((resolve, reject) => {
+      resolve(results);
+      reject(results);
+    });
+  });
 }
 
 /**
